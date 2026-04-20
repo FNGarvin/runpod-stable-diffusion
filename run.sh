54fad13ab00338cb00fa37bf311320fb9abf7726
Fred N. Garvin, Esq. <184324400+FNGarvin@users.noreply.github.com>
2026-04-20 10:44:22 -0500

    feat: add support for RunPod PUBLIC_KEY SSH injection

--- run.sh
+++ run.sh
@@ -3,6 +3,14 @@
 # Licensed under TODO: INSERT LICENSE
 # 2026
 
+# Set up SSH authorized keys if PUBLIC_KEY is provided (RunPod standard)
+if [ -n "$PUBLIC_KEY" ]; then
+    mkdir -p /root/.ssh
+    echo "$PUBLIC_KEY" >> /root/.ssh/authorized_keys
+    chmod 700 /root/.ssh
+    chmod 600 /root/.ssh/authorized_keys
+fi
+
 # Start SSH server
 service ssh start
 
